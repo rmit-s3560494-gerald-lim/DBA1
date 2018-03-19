3739ac9ec0ed5121f2c83b13386c3371a939f188
Gerald Lim <s3560494@student.rmit.edu.au>
2018-03-20 01:34:46 +1100

    bare parsing of csv into arrays, untested yet

--- derbyLoad.java
+++ derbyLoad.java
@@ -4,11 +4,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import java.util.ArrayList;
 import java.util.Properties;
-
 import com.sun.tools.internal.xjc.Driver;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class derbyLoad
 {
@@ -19,6 +21,45 @@ public class derbyLoad
     {
         new derbyLoad().go(args);
         System.out.println("Finished loading");
+
+        String csvFile = "/home/ec2-user/BUSINESS_NAMES_201803.csv";
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = "\t";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use tab as separator
+                String[] REGISTER_NAME = line.split(csvSplitBy);
+                String[] BN_NAME = line.split(csvSplitBy);
+                String[] BN_STATUS = line.split(csvSplitBy);
+                String[] BN_REG_DT = line.split(csvSplitBy);
+                String[] BN_CANCEL_DT = line.split(csvSplitBy);
+                String[] BN_RENEW_DT = line.split(csvSplitBy);
+                String[] BN_STATE_NUM = line.split(csvSplitBy);
+                String[] BN_STATE_OF_REG = line.split(csvSplitBy);
+                int[] BN_ABN = line.split(csvSplitBy);
+                // System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     void go(String [] args)
