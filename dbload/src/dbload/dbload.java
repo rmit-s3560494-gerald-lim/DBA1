5e502f0419aea7a20f3cc5c403d281c642d2b548
Gerald Lim <s3560494@student.rmit.edu.au>
2018-03-23 03:29:21 +1100

    reading of csv into array

---
+++ dbload/src/dbload/dbload.java
@@ -0,0 +1,48 @@
+package dbload;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class dbload {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		String fileName = "/Users/gerald/Downloads/BUSINESS_NAMES_201803.csv";
+		File file = new File(fileName);
+		
+		List<List<String>> lines = new ArrayList<>();
+		
+		Scanner inputStream;
+		
+		try {
+			inputStream = new Scanner(file);
+			
+			while(inputStream.hasNext()) {
+				String line = inputStream.next();
+				String[] values = line.split("\t");
+				lines.add(Arrays.asList(values));
+			}
+			inputStream.close();
+		} catch (FileNotFoundException a) {
+			a.printStackTrace();
+		}
+		
+		int lineNo = 1;
+		for (List<String> line: lines)
+		{
+			int columnNo = 1;
+			for (String value: line)
+			{
+				System.out.println(lineNo + " " + columnNo + " " + value);
+				columnNo++;
+			}
+			lineNo++;
+		}
+		
+	}
+
+}
