387b26912bff07d7e103fab9101a42abd0fa321d
Gerald Lim <s3560494@student.rmit.edu.au>
2018-04-01 01:05:28 +1100

    removed useless import

--- P3/dbload.java
+++ P3/dbload.java
@@ -50,121 +50,116 @@ public class dbload
         byte[] t_BN_STATE_NUM;
         byte[] t_BN_STATE_OF_REG;
         byte[] t_BN_ABN;
-        
-        try
+
+        //File file = new File("/Users/gerald/Google Drive/Database Systems/A1/P3/bncsv.txt");
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        DataOutputStream os = new DataOutputStream(new FileOutputStream(newFilename));
+        String line;
+        // this skips the first line of the csv file
+        br.readLine();
+        while((line = br.readLine()) != null)
         {
-            //File file = new File("/Users/gerald/Google Drive/Database Systems/A1/P3/bncsv.txt");
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            DataOutputStream os = new DataOutputStream(new FileOutputStream(newFilename));
-            String line;
-            // this skips the first line of the csv file
-            br.readLine();
-            while((line = br.readLine()) != null)
+            String[] value = line.split("\t");
+            BN_NAME = value[1];
+            BN_STATUS = value[2];
+            BN_REG_DT = value[3];
+            BN_CANCEL_DT = value[4];
+            BN_RENEW_DT = value[5];
+
+            // set to null if string is empty
+            try {
+                BN_STATE_NUM = value[6];
+            } catch (IndexOutOfBoundsException i)
             {
-                String[] value = line.split("\t");
-                BN_NAME = value[1];
-                BN_STATUS = value[2];
-                BN_REG_DT = value[3];
-                BN_CANCEL_DT = value[4];
-                BN_RENEW_DT = value[5];
+                BN_STATE_NUM = "";
+            }
+            try {
+                BN_STATE_OF_REG = value[7];
+            } catch (IndexOutOfBoundsException i)
+            {
+                BN_STATE_OF_REG = "";
+            }
+            try {
+                BN_ABN = value[8];
+            } catch (IndexOutOfBoundsException i)
+            {
+                BN_ABN = "";
+            }
 
-                // set to null if string is empty
-                try {
-                    BN_STATE_NUM = value[6];
-                } catch (IndexOutOfBoundsException i)
+            b_BN_NAME = BN_NAME.getBytes("UTF-8");
+            byte[] t_BN_NAME = Arrays.copyOf(b_BN_NAME, 200);
+            b_BN_STATUS = BN_STATUS.getBytes("UTF-8");
+            byte[] t_BN_STATUS = Arrays.copyOf(b_BN_STATUS, 12);
+            b_BN_REG_DT = BN_REG_DT.getBytes("UTF-8");
+            byte[] t_BN_REG_DT = Arrays.copyOf(b_BN_REG_DT, 10);
+            b_BN_CANCEL_DT = BN_CANCEL_DT.getBytes("UTF-8");
+            byte[] t_BN_CANCEL_DT = Arrays.copyOf(b_BN_CANCEL_DT, 10);
+            b_BN_RENEW_DT = BN_RENEW_DT.getBytes("UTF-8");
+            byte[] t_BN_RENEW_DT = Arrays.copyOf(b_BN_RENEW_DT, 10);
+            // set to null if it was previously null
+            try 
+            {
+                b_BN_STATE_NUM = BN_STATE_NUM.getBytes("UTF-8");
+                t_BN_STATE_NUM = Arrays.copyOf(b_BN_STATE_NUM, 10);
+            } catch (NullPointerException i)
+            {
+                b_BN_STATE_NUM = null;
+                t_BN_STATE_NUM = Arrays.copyOf(b_BN_STATE_NUM, 10);
+            }
+            try 
+            {
+                b_BN_STATE_OF_REG = BN_STATE_OF_REG.getBytes("UTF-8");
+                t_BN_STATE_OF_REG = Arrays.copyOf(b_BN_STATE_OF_REG, 3);
+            } catch (NullPointerException i)
+            {
+                b_BN_STATE_OF_REG = null;
+                t_BN_STATE_OF_REG = Arrays.copyOf(b_BN_STATE_OF_REG, 3);
+            }
+            try 
+            {
+                b_BN_ABN = BN_ABN.getBytes("UTF-8");
+                t_BN_ABN = Arrays.copyOf(b_BN_ABN, 20);
+            } catch (NullPointerException i)
+            {
+                b_BN_ABN = null;
+                t_BN_ABN = Arrays.copyOf(b_BN_ABN, 20);
+            }
+            
+            // each os.write(44); is a comma    
+            os.write(t_BN_NAME);
+            os.write(44);
+            os.write(t_BN_STATUS);
+            os.write(44);
+            os.write(t_BN_REG_DT);
+            os.write(44);
+            os.write(t_BN_CANCEL_DT);
+            os.write(44);
+            os.write(t_BN_RENEW_DT);
+            os.write(44);
+            os.write(t_BN_STATE_NUM);
+            os.write(44);
+            os.write(t_BN_STATE_OF_REG);
+            os.write(44);
+            os.write(t_BN_ABN);
+            count++;
+            
+            if(count == recordSize)
+            {
+                for(int i = 0; i < freeSpace; i ++)
                 {
-                    BN_STATE_NUM = "";
-                }
-                try {
-                    BN_STATE_OF_REG = value[7];
-                } catch (IndexOutOfBoundsException i)
-                {
-                    BN_STATE_OF_REG = "";
-                }
-                try {
-                    BN_ABN = value[8];
-                } catch (IndexOutOfBoundsException i)
-                {
-                    BN_ABN = "";
-                }
-
-                b_BN_NAME = BN_NAME.getBytes("UTF-8");
-                byte[] t_BN_NAME = Arrays.copyOf(b_BN_NAME, 200);
-                b_BN_STATUS = BN_STATUS.getBytes("UTF-8");
-                byte[] t_BN_STATUS = Arrays.copyOf(b_BN_STATUS, 12);
-                b_BN_REG_DT = BN_REG_DT.getBytes("UTF-8");
-                byte[] t_BN_REG_DT = Arrays.copyOf(b_BN_REG_DT, 10);
-                b_BN_CANCEL_DT = BN_CANCEL_DT.getBytes("UTF-8");
-                byte[] t_BN_CANCEL_DT = Arrays.copyOf(b_BN_CANCEL_DT, 10);
-                b_BN_RENEW_DT = BN_RENEW_DT.getBytes("UTF-8");
-                byte[] t_BN_RENEW_DT = Arrays.copyOf(b_BN_RENEW_DT, 10);
-                // set to null if it was previously null
-                try 
-                {
-                    b_BN_STATE_NUM = BN_STATE_NUM.getBytes("UTF-8");
-                    t_BN_STATE_NUM = Arrays.copyOf(b_BN_STATE_NUM, 10);
-                } catch (NullPointerException i)
-                {
-                    b_BN_STATE_NUM = null;
-                    t_BN_STATE_NUM = Arrays.copyOf(b_BN_STATE_NUM, 10);
-                }
-                try 
-                {
-                    b_BN_STATE_OF_REG = BN_STATE_OF_REG.getBytes("UTF-8");
-                    t_BN_STATE_OF_REG = Arrays.copyOf(b_BN_STATE_OF_REG, 3);
-                } catch (NullPointerException i)
-                {
-                    b_BN_STATE_OF_REG = null;
-                    t_BN_STATE_OF_REG = Arrays.copyOf(b_BN_STATE_OF_REG, 3);
-                }
-                try 
-                {
-                    b_BN_ABN = BN_ABN.getBytes("UTF-8");
-                    t_BN_ABN = Arrays.copyOf(b_BN_ABN, 20);
-                } catch (NullPointerException i)
-                {
-                    b_BN_ABN = null;
-                    t_BN_ABN = Arrays.copyOf(b_BN_ABN, 20);
-                }
-                
-                // each os.write(44); is a comma    
-                os.write(t_BN_NAME);
-                os.write(44);
-                os.write(t_BN_STATUS);
-                os.write(44);
-                os.write(t_BN_REG_DT);
-                os.write(44);
-                os.write(t_BN_CANCEL_DT);
-                os.write(44);
-                os.write(t_BN_RENEW_DT);
-                os.write(44);
-                os.write(t_BN_STATE_NUM);
-                os.write(44);
-                os.write(t_BN_STATE_OF_REG);
-                os.write(44);
-                os.write(t_BN_ABN);
-                count++;
-                
-                if(count == recordSize)
-                {
-                    for(int i = 0; i < freeSpace; i ++)
-                    {
-                        // write null to the spaces that are not filled up
-                        os.write(0);
-                    }
+                    // write null to the spaces that are not filled up
+                    os.write(0);
                 }
             }
-            br.close();
-            os.close();
-            // end timing
-            end = System.currentTimeMillis();
-            System.out.println("Number of records: " + count);
-            // simple count of pages
-            noPages = count/noRecords;
-            System.out.println("Number of pages: " + noPages);
-            System.out.println("Time taken to load is: "+ (end - start)+ "ms");
-        } catch (FileNotFoundException i){
-            i.printStackTrace();
         }
+        br.close();
+        os.close();
+        // end timing
+        end = System.currentTimeMillis();
+        System.out.println("Number of records: " + count);
+        // simple count of pages
+        noPages = count/noRecords;
+        System.out.println("Number of pages: " + noPages);
+        System.out.println("Time taken to load is: "+ (end - start)+ "ms");
     }
 }
