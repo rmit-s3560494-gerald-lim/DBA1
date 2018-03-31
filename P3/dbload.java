883aa04fdd1fb0e74bc82e8e12f3d835e2df30e9
Gerald Lim <s3560494@student.rmit.edu.au>
2018-03-31 22:28:48 +1100

    restructured dbload, single class will be fine

--- P3/dbload.java
+++ P3/dbload.java
@@ -1,5 +1,8 @@
 import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,30 +10,49 @@ import java.util.ArrayList;
 public class dbload {
 
     public static void main(String[] args) throws IOException {
-        /*int pageSize = Integer.valueOf(args[1]);
+        int pageSize = Integer.valueOf(args[1]);
         String fileName = args[2]; 
-        String newFilename = "heap." + pageSize;*/
+        String newFilename = "heap." + pageSize + ".dat";
         // file for testing
+        long start = System.currentTimeMillis();
+		long end;
+
         File file = new File("/Users/gerald/Google Drive/Database Systems/A1/P3/bncsv.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
+        DataOutputStream os = new DataOutputStream(new FileOutputStream(newFilename));
         String line;
         // this skips the first line of the csv file
         br.readLine();
-        ArrayList<Record> record = new ArrayList<Record>();
-        
+
+        int count = 0;
         while((line = br.readLine()) != null)
         {
             String[] value = line.split("\t");
-            char[] BN_NAME = value[1].toCharArray();
-            char[] BN_STATUS = value[2].toCharArray();
-            char[] BN_REG_DT = value[3].toCharArray();
-            char[] BN_CANCEL_DT = value[4].toCharArray();
-            char[] BN_RENEW_DT = value[5].toCharArray();
-            char[] BN_STATE_NUM = value[6].toCharArray();
-            char[] BN_STATE_OF_REG = value[7].toCharArray();
-            char[] BN_ABN = value[8].toCharArray();
-            record.add(new Record(BN_NAME, BN_STATUS, BN_REG_DT, BN_CANCEL_DT, BN_RENEW_DT, BN_STATE_NUM, BN_STATE_OF_REG, BN_ABN));
+            String BN_NAME = value[1];
+            String BN_STATUS = value[2];
+            String BN_REG_DT = value[3];
+            String BN_CANCEL_DT = value[4];
+            String BN_RENEW_DT = value[5];
+            String BN_STATE_NUM = value[6];
+            String BN_STATE_OF_REG = value[7];
+            String BN_ABN = value[8]; 
+
+            byte[] b_BN_NAME = BN_NAME.getBytes();
+            byte[] b_BN_STATUS = BN_STATUS.getBytes();
+            byte[] b_BN_REG_DT = BN_REG_DT.getBytes();
+            byte[] b_BN_CANCEL_DT = BN_CANCEL_DT.getBytes();
+            byte[] b_BN_RENEW_DT = BN_RENEW_DT.getBytes();
+            byte[] b_BN_STATE_NUM = BN_STATE_NUM.getBytes();
+            byte[] b_BN_STATE_OF_REG = BN_STATE_OF_REG.getBytes();
+            byte[] b_BN_ABN = BN_ABN.getBytes();
+
+            count++;
+            
         }
+        end = System.currentTimeMillis();
+        System.out.println("Number of records: " + count);
+		System.out.println("Time taken to load is: "+ (end - start)+ "ms");
+        br.close();
     }
 
 }
